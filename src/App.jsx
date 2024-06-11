a8a29b093c5fc69e45e693d627f4172df501d6bc
Nischal <nischalshrestha800@gmail.com>
2024-06-11 14:56:30 +0545

    project management

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home/Home'
 function App() {
 
   return (
-    // 2
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar'
+import axios from 'axios'
 const Home = () => {
+    const fetchBlogs = async () => {
+        const responce = await axios.get('http://localhost:2000/blogs')
+        console.log(responce);
+    }
+
+
+
     return (
         <div>
             {/* use of components Navbar */}
